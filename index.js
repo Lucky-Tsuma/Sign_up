3a76e2b6ca44cd92a6ffa98641b605f3fec02c3f
Lucky-Tsuma <tsumahmumba@gmail.com>
2021-11-01 21:07:59 +0300

    Write shorter code

--- index.js
+++ index.js
@@ -3,49 +3,35 @@ const app = express();
 const port = 3000;
 
 const users = [];
-const password_length = RegExp('(?=.{8,})');
-const password_lowercase = RegExp('(?=.*[a-z])');
-const password_caps = RegExp('(?=.*[A-Z])');
-const password_numbers = RegExp('(?=.*[0-9])');
-const password_special_symbols = RegExp('(?=.*[^A-Za-z0-9])');
+
+app.get('/', (req, res) => {
+    res.status(200).send("This is the homepage");
+});
+
+app.get('/users', (req, res) => {
+    res.status(200).send(users);
+});
 
 app.post('/sign_up', (req, res) => {
-    const email = req.query.email;
-    const username = req.query.username;
-    const password = req.query.password;
-    const confirm_password = req.query.confirm_password;
+    
+    const length = RegExp('(?=.{8,})');
+    const password_regex = RegExp('(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^A-Za-z0-9])');
+
+    const { email, username, password, confirm_password} = req.query;
 
     if(password != confirm_password) {
         res.status(400).send("Passwords do not match");
     }
 
-    if(!password_length.test(password)) {
-        res.status(400).send("Password too short");
+    if(!length.test(password)) {
+        res.status(400).send("Password should be at least 8 characters long");
     } 
 
-    if(!password_lowercase.test(password)) {
-        res.status(400).send("Password should contain lowercase characters");
+    if(!password_regex.test(password)) {
+        res.status(400).send("Password should contain both upper and lowercase characters, numbers and special characters");
     }
 
-    if(!password_caps.test(password)) {
-        res.status(400).send("Password should contain uppercase characters");
-    }
-
-    if(!password_numbers.test(password)) {
-        res.status(400).send("Password should contain numbers");
-    }
-
-    if(!password_special_symbols.test(password)) {
-        res.status(400).send("Password should contain special characters");
-    }
-
-    const user = {
-        "email" : email,
-        "username" : username,
-        "password" : password
-    }
-
-    users.push(user);
+    users.push({email, username, password});
     res.send(users);
 });
 
